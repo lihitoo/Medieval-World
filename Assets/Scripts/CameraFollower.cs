62f8b68d08858096f95051e56ded77f621cd1b12
lihitoo <trinhtuu.05@gmail.com>
2024-05-30 15:25:19 +0700

    Initial commit

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Transform target;
+    private float smoothTime = 0.3f;
+    [SerializeField] private Vector3 offset;
+     private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector2 move;
+    [SerializeField] private float moveSpeed = 8f;
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+    private void MovePlayer()
+    {
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+            transform.Translate(movement * Time.deltaTime * moveSpeed, Space.World);
+        }
+            
+    }
+}
